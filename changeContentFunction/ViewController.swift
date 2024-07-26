7c7e63f0b13fd68b03c634da213091a3e711ef1e
KevinHung <kevinaugust145@gmail.com>
2024-07-26 16:18:54 +0800

    Initial Commit

---
+++ changeContentFunction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  changeContentFunction
+//
+//  Created by 洪宗燦 on 2024/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ changeContentFunctionTests/changeContentFunctionTests.swift
@@ -0,0 +1,17 @@
+//
+//  changeContentFunctionTests.swift
+//  changeContentFunctionTests
+//
+//  Created by 洪宗燦 on 2024/7/26.
+//
+
+import Testing
+@testable import changeContentFunction
+
+struct changeContentFunctionTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
